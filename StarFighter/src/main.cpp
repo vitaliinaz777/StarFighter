2d4baa1cf671e3346105249a47144ae56d6f8e95
Vitalii Nazarov <vitaliinaz777@gmail.com>
2025-03-07 23:39:04 +0300

    Fetch SFML

--- StarFighter/src/main.cpp
+++ StarFighter/src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "hello world!" << std::endl;
+	sf::RenderWindow renderWindow{ sf::VideoMode(100,100), "Star Fighter" };
+
+	std::cout << "Star Fighter!!" << std::endl;
 }
