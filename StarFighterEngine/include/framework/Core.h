9422b18d87b0e9dd223d860a8abc44fd33767cac
Vitalii Nazarov <vitaliinaz777@gmail.com>
2025-03-09 23:41:45 +0300

    added aliases for Data Types

--- StarFighterEngine/include/framework/Core.h
+++ StarFighterEngine/include/framework/Core.h
@@ -2,8 +2,24 @@
 
 namespace st
 {
+    template<typename T>
+    using unique = std::unique_ptr<T>;
 
-//a macro
-#define LOG(M, ...) printf(M "\n", ##__VA_ARGS__)
+    template<typename T>
+    using shared = std::shared_ptr<T>;
 
+    template<typename T>
+    using weak = std::weak_ptr<T>;
+
+    template<typename T>
+    using List = std::vector<T>;
+
+    template<typename keyType, typename valType, typename Pr = std::less<keyType>>
+    using Map = std::map<keyType, valType, Pr>;
+
+    template<typename keyType, typename valType, typename hasher = std::hash<keyType>>
+    using Dictionary = std::unordered_map<keyType, valType, hasher>;
+
+    //a macro
+    #define LOG(M, ...) printf(M "\n", ##__VA_ARGS__)
 }
