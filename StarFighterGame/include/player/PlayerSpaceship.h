1e32c76574feb08219fd5c21e5229281c3590759
Vitalii Nazarov <vitaliinaz777@gmail.com>
2025-03-10 21:22:17 +0300

    added PlayerSpaceship class

---
+++ StarFighterGame/include/player/PlayerSpaceship.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "spaceship/Spaceship.h"
+
+namespace st
+{
+    // forward declaration
+    class BulletShooter;
+
+    class PlayerSpaceship : public Spaceship
+    {
+    public:
+        PlayerSpaceship(World* owningWorld, const std::string& path = "SpaceShooterRedux/PNG/playerShip1_blue.png");
+
+        virtual void Tick(float deltaTime) override;
+        void SetSpeed(float newSpeed) { mSpeed = newSpeed; }
+        float GetSpeed() const { return mSpeed; }
+        virtual void Shoot() override;
+    private:
+        void HandleInput();
+        void NormalizeInput();
+        void ClampInputOnEdge();
+        void ConsumeInput(float deltaTime);  // consume the input 
+        // and set the velocity of the spaceship
+        sf::Vector2f mMoveInput; // direction
+        float mSpeed;
+
+        unique<BulletShooter> mShooter;
+    };
+}
---
+++ StarFighterGame/src/player/PlayerSpaceship.cpp
@@ -0,0 +1,89 @@
+#include "player/PlayerSpaceship.h"
+#include "SFML/System.hpp"
+#include "framework/MathUtility.h"
+#include "weapon/BulletShooter.h"
+
+namespace st
+{
+    PlayerSpaceship::PlayerSpaceship(World* owningWorld, const std::string& path)
+        : Spaceship{ owningWorld, path },
+        mMoveInput{ 0.f, 0.f },
+        mSpeed{ 200.f },
+        mShooter{ new BulletShooter{this, 0.3f} }
+    {
+    }
+
+    void PlayerSpaceship::Tick(float deltaTime)
+    {
+        Spaceship::Tick(deltaTime);
+        HandleInput();
+        ConsumeInput(deltaTime);
+    }
+
+    void PlayerSpaceship::Shoot()
+    {
+        if (mShooter) {
+            mShooter->Shoot();
+        }
+    }
+
+    void PlayerSpaceship::HandleInput()
+    {
+        // Handle vertical movement
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            mMoveInput.y = -1.f; // up
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            mMoveInput.y = 1.f; // down
+        }
+
+        // Handle horizontal movement
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            mMoveInput.x = -1.f; // left
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            mMoveInput.x = 1.f; // right
+        }
+
+        ClampInputOnEdge();
+        NormalizeInput();
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+            Shoot();
+        }
+    }
+
+    void PlayerSpaceship::NormalizeInput()
+    {
+        Normalize(mMoveInput);
+        //LOG("Normalized input: %f, %f", mMoveInput.x, mMoveInput.y);
+    }
+
+    void PlayerSpaceship::ClampInputOnEdge()
+    {
+        sf::Vector2f actorLocation = GetActorLocation();
+        if (actorLocation.x < 0.f && mMoveInput.x == -1) {
+            mMoveInput.x = 0.f;
+        }
+
+        if (actorLocation.x > GetWindowsSize().x && mMoveInput.x == 1.f) {
+            mMoveInput.x = 0.f;
+        }
+
+        if (actorLocation.y < 0.f && mMoveInput.y == -1) {
+            mMoveInput.y = 0.f;
+        }
+
+        if (actorLocation.y > GetWindowsSize().y && mMoveInput.y == 1.f) {
+            mMoveInput.y = 0.f;
+        }
+    }
+
+    // Consume the input and set the velocity of the spaceship
+    void PlayerSpaceship::ConsumeInput(float deltaTime)
+    {
+        SetVelocity(mMoveInput * mSpeed); // 
+
+        // reset the input
+        mMoveInput.x = 0.f;
+        mMoveInput.y = 0.f;
+    }
+}
